8a731d48f27b5079a06cd5a7c89ea5be757bb521
Ivy Xu <xyan21418@gmail.com>
2024-11-05 18:49:46 +0200

    drew ship to bottom center of the screen; window closed on ESC key press

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,43 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main()
 {
-    auto window = sf::RenderWindow({1920u, 1080u}, "CMake SFML Project");
+    unsigned int windowWidth = 1200u;
+    unsigned int windowHeight = 800u;
+    auto window = sf::RenderWindow({windowWidth, windowHeight}, "Alien Invasion");
     window.setFramerateLimit(144);
+
+    sf::Texture texture;
+    if (!texture.loadFromFile("src/ship.png"))
+    {
+        std::cout << "Error loading texture\n";
+        return 1;
+    }
+
+    sf::Vector2u textureSize = texture.getSize();
+    unsigned int shipWidth = textureSize.x;
+    unsigned int shipHeight = textureSize.y;
+
+    sf::Sprite ship(texture);
+    ship.setPosition(sf::Vector2f((windowWidth - shipWidth ) / 2, windowHeight - shipHeight));
 
     while (window.isOpen())
     {
-        for (auto event = sf::Event(); window.pollEvent(event);)
+        sf::Event event;
+        while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
-            {
                 window.close();
+            if (event.type == sf::Event::KeyPressed)
+            {
+                if (event.key.code == sf::Keyboard::Escape)
+                    window.close();
             }
         }
 
-        window.clear();
+        window.clear(sf::Color::Black);
+        window.draw(ship);
         window.display();
     }
 }
